43236f59440db5b30b84f8f4e6c20ae5ef047350
ps202203 <pase375@gmail.com>
2025-03-25 21:17:18 +0900

    Ch 2-1 ~ 2-3. set ORM H2 JPA.

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbbApplication {
+public class  SbbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SbbApplication.class, args);
