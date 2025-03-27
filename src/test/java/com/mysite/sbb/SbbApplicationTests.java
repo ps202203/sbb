4de75935029ed4f7753244249893379c7240d2aa
ps202203 <pase375@gmail.com>
2025-03-27 17:23:06 +0900

    CH2-5, test junit. findById Method.

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -2,6 +2,7 @@ package com.mysite.sbb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
 
 import java.util.List;
 
@@ -19,10 +20,10 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2,all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
     }
 }
